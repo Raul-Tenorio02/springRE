1908befb29943314e564dcd0648cc6d90de56473
Raul Tenorio <raul.amaral2005@gmail.com>
2025-03-21 14:19:11 -0300

    frontend fix

--- src/main/java/dev/raul/springRE2/Controller/ItemViewController.java
+++ src/main/java/dev/raul/springRE2/Controller/ItemViewController.java
@@ -12,6 +12,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Controller
@@ -27,7 +29,8 @@ public class ItemViewController {
     //READ
     @GetMapping
     public String listItems(Model model) {
-        model.addAttribute("items", itemService.getAll());
+        List<Item> items = itemService.getAll();
+        model.addAttribute("items", items != null ? items : new ArrayList<>());
         return "items";
     }
 
